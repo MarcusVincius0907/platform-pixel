2650b09f132f8892bd4bf43d2b9baec6051a08ee
unknown <mvleite0908@gmail.com>
2023-04-18 16:11:30 -0300

    update deploy files

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,11 +6,6 @@ sudo chmod -R 777 /home/ec2-user/pixel/platform-pixel
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/pixel/platform-pixel
 
-#install node modules
-npm install
-
-npm install --prefix /home/ec2-user/pixel/platform-pixel/server
-
 #build app
 npm run build:cb
 
