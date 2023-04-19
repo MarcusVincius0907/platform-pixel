45881794316ab10c05abb930ad93ee8f20c6b6e2
unknown <mvleite0908@gmail.com>
2023-04-18 21:34:22 -0300

    deploy

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,10 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/pixel/platform-pixel
 
+if [ ! -d /home/ec2-user/pixel/platform-pixel ]; then
+  mkdir /home/ec2-user/pixel/platform-pixel
+fi
+
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/pixel/platform-pixel
 
